5f66945af9896d4e3fc7a73d4d9dd7fd306f5717
Ilya Shashilov <kvushisha@gmail.com>
2019-04-17 21:18:35 +0300

    add layout and site controller

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SiteController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('site/index.html.twig');
+    }
+}
